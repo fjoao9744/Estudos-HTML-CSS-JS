6ce5a2bcf2782e8fb73464d4c5a74bc5bdebe495
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-01-03 19:33:43 -0300

    formularios 02

--- Testes/formularios/02/script.js
+++ Testes/formularios/02/script.js
@@ -1,12 +1,11 @@
-const form = document.getElementById("Meuformulario"); // declara uma varivel que vai ser o formulario
+const form = document.getElementById("Form"); // declara uma variavel que vai ser o formulario
 
 form.addEventListener("submit", function(event) { // declara uma função que vai ser executada quando o evento submit for acionado
 
-    const data = new FormData(form)
-
-    console.log("Nome:", FormData.get("nome"))
-    console.log("Email:", FormData.get("email"))
-    console.log("Mensagem:", FormData.get("mensagem"))
-
+    const data = new FormData(event.target);
+    
+    const nome = data.get("nome");
+    const email = data.get("email");
+    const mensagem = data.get("mensagem");
 });
 
